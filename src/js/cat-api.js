21bca132f0a621d462ae60967b726cbce87bb4b3
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2023-10-08 11:26:55 +0300

    refactor: Remove unused environment variable configuration and Axios header setup

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,11 +1,5 @@
 import axios from 'axios';
 
-// require('dotenv').config();
-
-// const apiKey = process.env.CAT_API;
-// const BASE_URL = process.env.BASE_URL;
-// axios.defaults.headers.common['x-api-key'] = apiKey;
-
 const API_KEY =
   'live_dr2XvSppj5H2JReBAkEQw0jmlHgQrHoVBPUNZt2mqGPLPYjE7JnBCT19x58KHIf6';
 const BASE_URL = 'https://api.thecatapi.com/v1';
